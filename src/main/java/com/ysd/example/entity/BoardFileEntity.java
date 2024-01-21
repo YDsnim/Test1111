444febc840bec0ceefec4e1fd67ddfa0fcbebe98
YDsnim <145634601+YDsnim@users.noreply.github.com>
2024-01-21 09:27:36 +0900

    example Project to Github Test1111 repo
    211212

---
+++ src/main/java/com/ysd/example/entity/BoardFileEntity.java
@@ -0,0 +1,15 @@
+package com.ysd.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//@Entity
+@Table
+@NoArgsConstructor
+@Getter
+@Setter
+public class BoardFileEntity {
+}
